f96bfb7d4d18c9e4270fbd2a22be396786c77149
Campbell Cole <cmcoleco@gmail.com>
2018-06-10 15:28:19 -0700

    basic discord

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // 'require's
 const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 3000 });
-
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
 const fs = require('fs');
 
 // init
@@ -12,4 +8,28 @@ const fs = require('fs');
 console.log("Developed by Campbell Cole");
 console.log("If you do not have a tensorflow anaconda environment set up, this will not work.");
 
+const ws = new WebSocket.Server({ port: 3000 });
+
+const client = new Discord.Client();
 const TOKEN = fs.readFileSync('common/discord-token.txt', 'utf8');
+client.login(TOKEN);
+
+// vars
+
+var discordConnected = false;
+var socketConnected = false;
+
+// discord
+
+client.on('ready', () => { discordConnected=true; });
+
+client.on('message', message => {
+  if (msg.content.startsWith('det:')) {
+    if (socketConnected) {
+      
+    } else {
+      message.channel.send("The Neural Network is not running!\n"
+      + "(socket not connected)");
+    }
+  }
+});
