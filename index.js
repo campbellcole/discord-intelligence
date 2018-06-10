cc96e3f37846d6dc298f1fb5197c64c961470b9a
Campbell Cole <cmcoleco@gmail.com>
2018-06-10 15:56:21 -0700

    socket and discord basic done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const fs = require('fs');
 console.log("Developed by Campbell Cole");
 console.log("If you do not have a tensorflow anaconda environment set up, this will not work.");
 
-const ws = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ port: 3000 });
 
 const client = new Discord.Client();
-const TOKEN = fs.readFileSync('common/discord-token.txt', 'utf8');
+const TOKEN = fs.readFileSync('common/discord-token.txt', 'utf8').trim();
 client.login(TOKEN);
 
 // vars
@@ -19,17 +19,47 @@ client.login(TOKEN);
 var discordConnected = false;
 var socketConnected = false;
 
+var channel = null; // single channel bot only
+
+var pending = false; // one command at a time
+
 // discord
+
+function sendDiscordMessage(text) {
+  if (channel != null) {
+    channel.send(text);
+  }
+}
 
 client.on('ready', () => { discordConnected=true; });
 
 client.on('message', message => {
-  if (msg.content.startsWith('det:')) {
+  if (message.content.startsWith('det:')) {
+    if (channel == null) channel = message.channel;
     if (socketConnected) {
-      
+      if (!pending) {
+        sendSocketMessage(message.content.substr(4));
+        pending = true;
+      } else {
+        sendDiscordMessage("Operation currently pending. Please wait.")
+      }
     } else {
       message.channel.send("The Neural Network is not running!\n"
       + "(socket not connected)");
     }
   }
+});
+
+// websocket
+
+function sendSocketMessage(text) {
+  wss.clients[0].send(text); // will never be multiple clients
+}
+
+wss.on('connection', ws => {
+  socketConnected = true;
+  ws.on('message', function incoming(message) {
+    sendDiscordMessage(message);
+    pending = false;
+  });
 });
