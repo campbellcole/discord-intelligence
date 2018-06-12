00af0b7b8f847d7f471b50c0c7d84c354ca4ce33
Campbell Cole <cmcoleco@gmail.com>
2018-06-11 18:19:22 -0700

    imported python and almost got it working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const WebSocket = require('ws');
 const Discord = require('discord.js');
 const fs = require('fs');
+const PythonShell = require('python-shell');
 
 // init
 
@@ -15,9 +16,12 @@ client.login(TOKEN);
 console.log("Developed by Campbell Cole");
 console.log("If you do not have a tensorflow anaconda environment set up, this will not work.");
 
+startNet();
+
 // vars
 
 var socketConnected = false;
+var discordConnected = false;
 var channel = null; // single channel bot only
 var pending = false; // one command at a time
 
@@ -30,6 +34,7 @@ function sendDiscordMessage(text) {
 }
 
 client.on('ready', () => {
+  discordConnected = true;
   console.log('logged in to discord: ' + client.user.tag);
   client.user.setActivity("hentai", {type:"WATCHING"});
   key = newKey();
@@ -48,8 +53,7 @@ client.on('message', message => {
 
 function sendSocketMessage(text) {
   if (!socketConnected) {
-    sendDiscordMessage("The Neural Network is not running!\n"
-                      + "socketConnected = false");
+    sendDiscordMessage("The Neural Network is not running!");
     return;
   }
   if (!pending) {
@@ -67,6 +71,7 @@ function sendSocketMessage(text) {
 wss.on('connection', ws => {
   console.log('connected to neural network')
   socketConnected = true;
+  if (discordConnected) sendDiscordMessage('Socket connected successfully.');
   ws.on('message', function incoming(message) {
     sendDiscordMessage(message);
     pending = false;
@@ -80,12 +85,16 @@ var key = "";
 function handleCommand(message) {
   var msg = message.content.substr(4);
   var args = msg.split(" ");
-  var cmd = args.splice(0,1)[0];
+  var cmd = args.splice(0, 1)[0];
   switch (cmd) {
     case "help":
       sendDiscordMessage("no lmao");
       break;
     case "link":
+      if (message.channel == null) {
+        console.log('cannot be run from console');
+        break;
+      }
       channel = message.channel;
       sendDiscordMessage("Linked to current channel. (If you DM me commands, the outputs will appear here)");
       break;
@@ -98,6 +107,14 @@ function handleCommand(message) {
                         + "\nargs = " + args
                         + "\nchannel = " + channel
                         + "\nmessage.author = " + message.author);
+      break;
+    case "forcepending":
+      var willBePending = args[0]=='true';
+      if (args[1] == key) {
+        pending = willBePending;
+        sendDiscordMessage("Forced 'pending' to " + pending);
+        key = newKey();
+      }
       break;
     case "generate":
       if (parseInt(args[0]) > 200 && args[2] != key) {
@@ -121,20 +138,54 @@ function newKey() {
   return text;
 }
 
+// commandline input
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+process.stdin.on('data', function (text) {
+  if (channel == null) {
+    console.log('channel not set, link to a channel in client');
+    return;
+  }
+  handleCommand({content:"det:"+text.trim(), channel:null, author:"console"});
+});
+
 // neural network (python script)
 
 //TODO: control the script from here instead of manually
 
-//const shell = require('shelljs');
+var pyshell = null;
+
+// -data_dir=common/study-data.txt -alphabet_dir=common/alphabet.txt -mode=gen -model=common/network.hdf5
+
+// -data_dir=common/study-data.txt -alphabet_dir=common/alphabet.txt -mode=train -model=common/network.hdf5 -epochs=x -retrain=y
+var trainArgs = ["--data_dir=common/study-data.txt", "--alphabet_dir=common/alphabet.txt", "--mode=train", "--model=common/network.hdf5"];
 
 function startNet() {
+  var genArgs = ["--data_dir=common/study-data.txt", "--alphabet_dir=common/alphabet.txt", "--mode=gen", "--model=common/network.hdf5"];
+  /*if (!fs.existsSync("common/network.hdf5")) {
+    console.log("network save file does not exist, you must begin training first");
+    if (discordConnected) sendDiscordMessage("Could not start neural network. Checkpoint does not exist. Train the network.");
+    return;
+  }*/
+  console.log('starting neural network with web socket');
+  pyshell = new PythonShell('python/main.py', { pythonOptions: ["-u"], args: genArgs });
+  pyshell.on('error', function (message) {
+    console.log(message);
+  });
 
 }
 
 function stopNet() {
-
+  if (!pyshell.terminated) pyshell.terminate();
+  else console.log("network already terminated");
 }
 
-function trainNet(epochs = 50) {
-
+function trainNet(epochs = 50, reset = false) {
+  console.log('training network with ' + epochs + " epochs.");
+  stopNet();
+  trainArgs.push("-epochs="+epochs);
+  if (reset) trainArgs.push('-retrain=True');
+  pyshell = new PythonShell('python/main.py', trainArgs);
 }
--- python/main.py
+++ python/main.py
@@ -1,1 +1,115 @@
-##TODO: transfer and refactor code to here
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import csv
+import os
+import asyncio
+import sys
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.wrappers import TimeDistributed
+import argparse
+from RNN_utils import *
+import warnings
+warnings.filterwarnings('ignore')
+
+print("arguments: "+"\n".join(sys.argv));
+# Parsing arguments for Network definition
+ap = argparse.ArgumentParser()
+ap.add_argument('--data_dir', default="./data/test.txt")
+ap.add_argument('--alphabet_dir', default='./data/alphabet.txt')
+ap.add_argument('--batch_size', type=int, default=50)
+ap.add_argument('--layer_num', type=int, default=2)
+ap.add_argument('--seq_length', type=int, default=50)
+ap.add_argument('--hidden_dim', type=int, default=500)
+ap.add_argument('--generate_length', type=int, default=500)
+ap.add_argument('--nb_epoch', type=int, default=20)
+ap.add_argument('--mode', default='train')
+ap.add_argument('--model', default='')
+ap.add_argument('--retrain', type=bool, default=False)
+ap.add_argument('--epochs', type=int, default=50)
+args = vars(ap.parse_args())
+
+DATA_DIR = args['data_dir']
+ALPHABET_DIR = args['alphabet_dir']
+BATCH_SIZE = args['batch_size']
+HIDDEN_DIM = args['hidden_dim']
+SEQ_LENGTH = args['seq_length']
+MODEL = args['model']
+
+GENERATE_LENGTH = args['generate_length']
+LAYER_NUM = args['layer_num']
+
+RETRAIN = args['retrain']
+
+EPOCHS = args['epochs']
+
+X, y, VOCAB_SIZE, ix_to_char, chars = load_data(DATA_DIR, ALPHABET_DIR, SEQ_LENGTH)
+
+if not MODEL == '' and not RETRAIN:
+  model = load_model(MODEL)
+  print('model loaded\n')
+else:
+  model = Sequential()
+  model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
+  for i in range(LAYER_NUM - 1):
+    model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+  model.add(TimeDistributed(Dense(VOCAB_SIZE)))
+  model.add(Activation('softmax'))
+  model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+nb_epoch = 0 # this means nothing now because i'm not saving the epoch number
+
+# Training if there is no trained weights specified
+if args['mode'] == 'train':
+    if (RETRAIN):
+        try:
+            os.remove(os.getcwd() + '\\checkpoint.hdf5')
+        except Exception:
+            pass
+    while True:
+        print('\n\nEpoch: {}\n'.format(nb_epoch))
+        model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, nb_epoch=1)
+        nb_epoch += 1
+        if nb_epoch == EPOCHS:
+            model.save('checkpoint.hdf5', overwrite=True)
+            exit()
+
+
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+def on_open(ws):
+    print('connected to client')
+
+def on_message(ws, message):
+    print("received message: " + message)
+    split = message.split(" ");
+    command = split[0]
+    if command == "generate":
+        genlen = None
+        initx = None
+        try:
+            genlen = int(split[1])
+        except ValueError:
+            genlen = 10
+        try:
+            if split[2] in chars:
+                initx = chars.index(split[2])
+        except Exception:
+            pass
+        print("generating " + str(genlen) + " characters")
+        gen = generate_text(model, genlen, VOCAB_SIZE, ix_to_char, initx)
+        ws.send(gen)
+
+websocket.enableTrace(True)
+ws = websocket.WebSocketApp("ws://localhost:3000",
+                            on_message = on_message)
+ws.on_open = on_open
+ws.run_forever()
